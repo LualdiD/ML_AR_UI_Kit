fa3c09eeaee5607215d743eafe8d0d832228270a
Dylan <dylan.lualdi@hotmail.com>
2019-03-23 09:03:16 +0000

    Use CoreML Inceptionv3 in ARKit view to recognise objects

---
+++ ML_AR_UI_Kit/Controller/ViewController.swift
@@ -0,0 +1,252 @@
+//
+//  ViewController.swift
+//  ML_AR_UI_Kit
+//
+//  Created by Dylan Lualdi on 22/03/2019.
+//  Copyright © 2019 Dylan Lualdi. All rights reserved.
+//
+
+
+import UIKit
+import SpriteKit
+import ARKit
+import Vision
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate, ARSCNViewDelegate, ARSessionDelegate {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    // The view controller that displays the status and "restart experience" UI.
+    private lazy var statusViewController: StatusViewController = {
+        return children.lazy.compactMap({ $0 as? StatusViewController }).first!
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Set the session delegate
+        sceneView.session.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = false
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Enable Default Lighting - makes the 3D text a bit poppier.
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Hook up status view controller callback.
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartSession()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+    // MARK: - ARSessionDelegate
+    
+    // Pass camera frames received from ARKit to Vision (when not already processing one)
+    /// - Tag: ConsumeARFrames
+    
+   
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // Do not enqueue other buffers for processing while another Vision task is still running.
+        // The camera stream has only a finite amount of buffers available; holding too many buffers for analysis would starve the camera.
+        guard currentBuffer == nil, case .normal = frame.camera.trackingState else {
+            return
+        }
+        
+        // Retain the image buffer for Vision processing.
+        self.currentBuffer = frame.capturedImage
+        classifyCurrentImage()
+        
+    }
+    
+    // MARK: - Vision classification
+    
+    // Vision classification request and model
+    /// - Tag: ClassificationRequest
+    private lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            
+            print("classify")
+            // Instantiate the model from its generated Swift class.
+            let model = try VNCoreMLModel(for: Inceptionv3().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            
+            // Crop input images to square area at center, matching the way the ML model was trained.
+            request.imageCropAndScaleOption = .centerCrop
+            
+            // Use CPU for Vision processing to ensure that there are adequate GPU resources for rendering.
+            request.usesCPUOnly = true
+            
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    // The pixel buffer being held for analysis; used to serialize Vision requests.
+    private var currentBuffer: CVPixelBuffer?
+    
+    // Queue for dispatching vision classification requests
+    private let visionQueue = DispatchQueue(label: "com.example.apple-samplecode.ARKitVision.serialVisionQueue")
+    
+    // Run the Vision+ML classifier on the current image buffer.
+    /// - Tag: ClassifyCurrentImage
+    private func classifyCurrentImage() {
+        // Most computer vision tasks are not rotation agnostic so it is important to pass in the orientation of the image with respect to device.
+        let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
+        
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: currentBuffer!, orientation: orientation)
+        visionQueue.async {
+            do {
+                // Release the pixel buffer when done, allowing the next buffer to be processed.
+                defer { self.currentBuffer = nil }
+                try requestHandler.perform([self.classificationRequest])
+            } catch {
+                print("Error: Vision request failed with error \"\(error)\"")
+            }
+        }
+    }
+    
+    // Classification results
+    private var identifierString = ""
+    private var confidence: VNConfidence = 0.0
+    
+    // Handle completion of the Vision request and choose results to display.
+    /// - Tag: ProcessClassifications
+    func processClassifications(for request: VNRequest, error: Error?) {
+        guard let results = request.results else {
+            print("Unable to classify image.\n\(error!.localizedDescription)")
+            return
+        }
+        // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+        let classifications = results as! [VNClassificationObservation]
+        
+        // Show a label for the highest-confidence result (but only above a minimum confidence threshold).
+        if let bestResult = classifications.first(where: { result in result.confidence > 0.4 }),
+            let label = bestResult.identifier.split(separator: ",").first {
+            identifierString = String(label)
+            confidence = bestResult.confidence
+        } else {
+            identifierString = ""
+            confidence = 0
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.displayClassifierResults()
+        }
+    }
+    
+    // Show the classification results in the UI.
+    private func displayClassifierResults() {
+        guard !self.identifierString.isEmpty else {
+            return // No object was classified.
+        }
+        let message = String(format: "Detected \(self.identifierString) with %.2f", self.confidence * 100) + "% confidence"
+        statusViewController.showMessage(message)
+    }
+    
+    
+    // MARK: - AR Session Handling
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        statusViewController.showTrackingQualityInfo(for: camera.trackingState, autoHide: true)
+        
+        switch camera.trackingState {
+        case .notAvailable, .limited:
+            statusViewController.escalateFeedback(for: camera.trackingState, inSeconds: 3.0)
+        case .normal:
+            statusViewController.cancelScheduledMessage(for: .trackingStateEscalation)
+        }
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        
+        // Filter out optional error messages.
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) { }
+    
+    func sessionShouldAttemptRelocalization(_ session: ARSession) -> Bool {
+        /*
+         Allow the session to attempt to resume after an interruption.
+         This process may not succeed, so the app must be prepared
+         to reset the session if the relocalizing status continues
+         for a long time -- see `escalateFeedback` in `StatusViewController`.
+         */
+        return true
+    }
+    
+    private func restartSession() {
+        statusViewController.cancelAllScheduledMessages()
+        statusViewController.showMessage("RESTARTING SESSION")
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    // MARK: - Error handling
+    
+    private func displayErrorMessage(title: String, message: String) {
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.restartSession()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+// Convert device orientation to image orientation for use by Vision analysis.
+extension CGImagePropertyOrientation {
+    init(_ deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portraitUpsideDown: self = .left
+        case .landscapeLeft: self = .up
+        case .landscapeRight: self = .down
+        default: self = .right
+        }
+    }
+}
